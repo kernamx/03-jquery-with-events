77383184ffd66d6c60d16e8fa73afb171dd25686
kernamx <powen.chang1@gmail.com>
2017-08-16 14:21:36 -0700

    kevin drive, mark navigate

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,11 +40,14 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
-
+      $('article').hide();
+      var author = $(this).val();
+      $('article[data-author="' + author + '"]').show();
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
-
+      $('article').show();
+      $('article.template').hide();
     }
     $('#category-filter').val('');
   });
@@ -84,5 +87,6 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $(document).ready(function() {
-
+  articleView.populateFilters();
+  articleView.handleAuthorFilter();
 })
